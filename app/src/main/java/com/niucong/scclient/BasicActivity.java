657da8f5580188a3febc40c978f2c18906bb5a8f
牛聪 <niucong@julong.cc>
2019-08-01 17:20:54 +0800

    修改Androidx迁移bug、引用zBarLibary

--- app/src/main/java/com/niucong/scclient/BasicActivity.java
+++ app/src/main/java/com/niucong/scclient/BasicActivity.java
@@ -6,9 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -31,6 +30,10 @@ import com.niucong.zbar.CaptureActivity;
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import cn.bertsir.zbar.QrConfig;
+import cn.bertsir.zbar.QrManager;
 import rx.Observable;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
@@ -125,8 +128,8 @@ public abstract class BasicActivity extends AppCompatActivity implements View.On
                     searchType = 0;
                 }
                 if (str.length() > 0) {
-//                    iv_delete.setVisibility(View.VISIBLE);
-//                    iv_scan.setVisibility(View.GONE);
+                    iv_delete.setVisibility(View.VISIBLE);
+                    iv_scan.setVisibility(View.GONE);
                     try {
                         Long.valueOf(str);
                         if (str.length() > 11) {
@@ -136,8 +139,8 @@ public abstract class BasicActivity extends AppCompatActivity implements View.On
 
                     }
                 } else {
-//                    iv_delete.setVisibility(View.GONE);
-//                    iv_scan.setVisibility(View.VISIBLE);
+                    iv_delete.setVisibility(View.GONE);
+                    iv_scan.setVisibility(View.VISIBLE);
                     et_search.requestFocus();
                 }
             }
@@ -290,7 +293,43 @@ public abstract class BasicActivity extends AppCompatActivity implements View.On
 //                integrator.setCaptureActivity(ScanActivity.class);
 //                integrator.setCameraId(App.app.share.getIntMessage("SC", "CameraId", 0)); //1前置或者0后置摄像头
 //                integrator.initiateScan();
-                goScan();
+//                goScan();
+                QrConfig qrConfig = new QrConfig.Builder()
+                        .setDesText("")//扫描框下文字
+//                        .setShowDes(false)//是否显示扫描框下面文字
+                        .setShowLight(true)//显示手电筒按钮
+                        .setShowTitle(true)//显示Title
+                        .setShowAlbum(false)//是否显示从相册选择按钮
+                        .setCornerColor(Color.WHITE)//设置扫描框颜色
+                        .setLineColor(Color.WHITE)//设置扫描线颜色
+                        .setLineSpeed(QrConfig.LINE_MEDIUM)//设置扫描线速度
+                        .setScanType(QrConfig.TYPE_BARCODE)//设置扫码类型（二维码，条形码，全部，自定义，默认为二维码）
+                        .setScanViewType(QrConfig.SCANVIEW_TYPE_BARCODE)//设置扫描框类型（二维码还是条形码，默认为二维码）
+//                        .setCustombarcodeformat(QrConfig.BARCODE_I25)//此项只有在扫码类型为TYPE_CUSTOM时才有效
+                        .setPlaySound(true)//是否扫描成功后bi~的声音
+//                        .setNeedCrop(true)//从相册选择二维码之后再次截取二维码
+                        .setDingPath(R.raw.qrcode)//设置提示音(不设置为默认的Ding~)
+                        .setIsOnlyCenter(true)//是否只识别框中内容(默认为全屏识别)
+                        .setTitleText("药品条形码")//设置Tilte文字
+                        .setTitleBackgroudColor(getResources().getColor(R.color.colorPrimary))//设置状态栏颜色
+                        .setTitleTextColor(Color.WHITE)//设置Title文字颜色
+                        .setShowZoom(false)//是否手动调整焦距
+                        .setAutoZoom(false)//是否自动调整焦距
+                        .setFingerZoom(false)//是否开始双指缩放
+                        .setScreenOrientation(QrConfig.SCREEN_PORTRAIT)//设置屏幕方向
+//                        .setDoubleEngine(false)//是否开启双引擎识别(仅对识别二维码有效，并且开启后只识别框内功能将失效)
+//                        .setOpenAlbumText("选择要识别的图片")//打开相册的文字
+//                        .setLooperScan(false)//是否连续扫描二维码
+                        .create();
+                QrManager.getInstance().init(qrConfig).startScan(this, new QrManager.OnScanResultCallback() {
+                    @Override
+                    public void onScanSuccess(String result) {
+                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                        if (searchDrug(result)) {
+                            et_search.setText("");
+                        }
+                    }
+                });
                 break;
         }
     }
